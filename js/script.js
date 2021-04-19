6765c52fcc228c9b8d72584579dab6588161202f
Michael Silva <msilva@aggienetwork.com>
2021-04-19 18:15:57 -0400

    created initial files and HTML structure. Added Javascript to target elements in input and output.

---
+++ js/script.js
@@ -0,0 +1,4 @@
+var downInput = document.getElementById("downInput");
+var distanceInput = document.getElementById("distanceInput");
+var yardInput = document.getElementById("yardInput");
+var conversionOutput = document.getElementById("conversionOutput");
